5b7cf229de95f9a846c76ad20d6f084f6ae7fbfd
hugo salazar <hugog.salazarl@gmail.com>
2024-11-04 14:58:21 -0500

    chore: added randomize color grid when hovering

--- script.js
+++ script.js
@@ -12,16 +12,25 @@ gameInfoContainer.appendChild(gridInfo);
 button.addEventListener("click", getNumberOfDivs);
 function insertXDivsInContainer(number) {
 
-  for (let i = 0; i < number*number; i++) {
+  for (let i = 0; i < number * number; i++) {
     const div = document.createElement("div");
     div.style.flex = `1 0 calc(100% / ${number})`;
+    div.addEventListener("mouseover", () => {
+      const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      div.style.backgroundColor = randomColor;
+    });
     container.appendChild(div);
   }
 }
 
 function changeGameInfo(number) {
-const gridInfo = document.querySelector("h2");
-  gridInfo.innerText = `${number} * ${number}`
+  const gridInfo = document.querySelector("h2");
+  if (number == null) {
+    gridInfo.innerText = "You have to write a number";
+  } else {
+    gridInfo.innerText = `${number} * ${number}`
+  }
+
 
 }
 function getNumberOfDivs() {
